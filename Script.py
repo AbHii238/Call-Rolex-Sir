8ebe26360927ccb14b26dcbb755db71831944166
AbHii2255 <103345311+AbHii2255@users.noreply.github.com>
2022-09-26 13:48:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,7 +13,7 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: <b>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</b>
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: <b>𝙷𝙴𝚁𝙾𝙺𝚄</b>
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: <b>v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b>"""
-    SOURCE_TXT = """<b>✯Modified By➡️</b> <a href=https://t.me/AbHii2255><b>➝⃝ᴀʙʜɪᴊɪᴛʜ࿐</b></a>
+    SOURCE_TXT = """<b>✯Modified By➡️</b> <a href=https://t.me/AbHii238><b>➝⃝ᴀʙʜɪᴊɪᴛʜ࿐</b></a>
 
 <b>©TeamEvaMaria<b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
